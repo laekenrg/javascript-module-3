42f131810bec6f82436a1a70c099b54de37eb18d
laekenrg <laeken.rosado@gmail.com>
2022-04-23 18:58:17 +0200

    ejercicios terminados

--- ES 6 and beyond/src/17-exercise/17-exercise.js
+++ ES 6 and beyond/src/17-exercise/17-exercise.js
@@ -21,7 +21,9 @@
  */
 
 // Finish the code of the function
-function exercise17() {}
+function exercise17() {
+    fetch("https://jsonplaceholder.typicode.com/users/1").then(resp=> resp.json()).then(displayContents)
+}
 
 // Don’t change the code bellow this line
 function displayContents(data) {
